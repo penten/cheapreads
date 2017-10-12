4e01a190495d8e327b60e4bdd013ffbbe670e209
Max Martin <max@maxjmartin.com>
2017-10-12 11:34:33 +0000

    Add goodreads api code

---
+++ goodreads.php
@@ -0,0 +1,44 @@
+<?php
+
+function parseDate($dt) {
+	if($dt) return new DateTime($dt);
+	return false;
+}
+
+function getShelf($shelf) {
+	$key = $_SERVER['GOODREADS_KEY'];
+	$uid = $_SERVER['GOODREADS_UID'];
+
+	$page = 1;
+	$per_page = 100;
+	$books = [];
+	$total = 0;
+
+	$url = "https://www.goodreads.com/review/list/$uid.xml?key=$key&v=2&shelf=$shelf&per_page=$per_page";
+
+	do {
+		$xmlstr = file_get_contents("$url&page=$page");
+		$xml = new SimpleXMLElement($xmlstr);
+		# $total = $xml->reviews->attributes()->total;
+	
+		foreach($xml->reviews->review as $book) {
+			$id = (string)$book->book->id;
+
+			$books[$id] = [
+				"goodreads_id" => $id,
+				"my_rating" => (string)$book->rating,
+				"read_dt" => parseDate((string)$book->read_at),
+				"started_dt" => parseDate((string)$book->started_at),
+				"title" => (string)$book->book->title,
+				"image" => (string)$book->book->small_image_url,
+				"isbn" => (string)$book->book->isbn,
+				"isbn13" => (string)$book->book->isbn13,
+				"rating" => (string)$book->book->average_rating,
+				"author" => (string)$book->book->authors->author->name
+			];
+		}
+		$page++;
+	} while(count($xml->reviews->review));
+
+	return $books;
+}
